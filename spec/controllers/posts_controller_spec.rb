5a489f963457a6d3cc7e1d2d2152b10ffdc26cb7
Mykola Lubchenco <mykola.lubchenco@rolique.io>
2016-11-22 16:06:43 +0200

    fixed specs after review

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -4,11 +4,11 @@ RSpec.describe PostsController, type: :controller do
   let(:valid_attributes) {
     { title: 'some testing', body: 'lorem ipsum dolor', user_id: 1 }
   }
+  let(:post) { Post.create! valid_attributes }
 
   describe 'GET #publish' do
 
     it 'should become published when publish action called' do
-      post = Post.create! valid_attributes
       get :publish, params: { id: post.to_param, user_id: post.user_id }
 
       expect(assigns(:post).published).to eq(true)
@@ -22,34 +22,30 @@ RSpec.describe PostsController, type: :controller do
 
       get :unpublish, params: { id: post.to_param, user_id: post.user_id }
 
-      expect(assigns(:post).published).to_not eq(true)
+      expect(assigns(:post).published).to be_falsey
     end
   end
 
   describe 'with a published and user_id attribute' do
     it 'should become published when publish method called with correct user_id' do
-      post = Post.create! valid_attributes
       get :publish, params: { id: post.to_param, user_id: post.user_id }
 
       expect(assigns(:post).published).to eq(true)
     end
 
     it 'should show error when user_id is not correct' do
-      post = Post.create! valid_attributes
       get :publish, params: { id: post.to_param, user_id: post.user_id + 100 }
 
       expect(flash.notice).to eq('You do not have access to publish action')
     end
 
     it 'should not become published when user_id is not correct' do
-      post = Post.create! valid_attributes
       get :publish, params: { id: post.to_param, user_id: post.user_id + 100 }
 
       expect(assigns(:post).published).to eq(false)
     end
 
     it 'should redirect to homepage when user_id is not correct' do
-      post = Post.create! valid_attributes
       get :publish, params: { id: post.to_param, user_id: post.user_id + 100 }
 
       expect(response).to redirect_to(root_path)
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,29 +1,37 @@
+# frozen_string_literal: true
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
   context 'with a published attribute' do
-    before(:all) do
-      @post = Post.new(title: 'some testing', body: 'lorem ipsum dolor', user_id: 1)
-      expect(@post).to be_valid
+    let(:post) do
+      Post.new(title: 'some testing',
+               body: 'lorem ipsum dolor',
+               user_id: 1)
+    end
+
+    let(:post_published) do
+      Post.new(title: 'some testing',
+               body: 'lorem ipsum dolor',
+               user_id: 1,
+               published: true)
     end
 
     it 'should be unpublished when post created' do
-      expect(@post).to_not be_published
-    end
-
-    it 'should become published when publish method called' do
-      @post.publish
-      expect(@post).to be_published
-    end
-
-    it 'should become unpublished when unpublish method called' do
-      post = Post.new(title: 'some testing',
-                      body: 'lorem ipsum dolor',
-                      user_id: 1,
-                      published: true)
-
-      post.unpublish
       expect(post).to_not be_published
+    end
+
+    describe '.publish' do
+      context 'when publish' do
+        before { post.publish }
+        it { expect(post).to be_published }
+      end
+    end
+
+    describe '.unpublish' do
+      context 'when unpublish' do
+        before { post_published.unpublish }
+        it { expect(post_published).to_not be_published }
+      end
     end
   end
 end
